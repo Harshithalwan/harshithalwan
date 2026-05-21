d8537f32ddbf36b83aaafecf6df798435e55713b
harshithalwan-turing <harshit.h@turing.com>
2026-05-22 00:58:45 +0530

    Added logo and removed svelte-kit folder

--- .svelte-kit/generated/client/nodes/6.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/papers/[slug]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/papers/[slug]/+page.svelte";
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.pSnQPtLJ.js","_app/immutable/chunks/AMiBqiSu.js","_app/immutable/chunks/Ci4niXXX.js","_app/immutable/chunks/D6YF6ztN.js"];
-export const stylesheets = [];
-export const fonts = [];
